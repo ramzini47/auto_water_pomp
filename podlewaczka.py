6f62585eec24b5bb4659d391af1e69bf79bca9b8
ramzini47 <remigiuszglowacki+github@gmail.com>
2025-03-19 14:39:51 +0000

    Watering only if sensor decetcs dry soil

--- podlewaczka.py
+++ podlewaczka.py
@@ -4,7 +4,7 @@ import time
 
 # Set the GPIO numbering mode
 WATER_POMP = 18 # GPIO 24 - dziala
-VALVE_01 = 16 # 23
+#VALVE_01 = 16 # 23
 LED1 = 12  # GPIO 18 - dziala
 LED2 = 16  # GPIO 23 - cos nie dizala, robie 16 zeby sprawdzac jak zawor dziala
 MOISTURE_SENSOR_PIN = 16  # Physical Pin 11 (GPIO17)
@@ -15,7 +15,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED1, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(LED2, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(WATER_POMP, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(VALVE_01, GPIO.OUT, initial=GPIO.LOW)
+#GPIO.setup(VALVE_01, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(MOISTURE_SENSOR_PIN, GPIO.IN)
 
 # Function to turn both LEDs on or off
@@ -29,19 +29,19 @@ def blink_leds(times):
         time.sleep(0.5)  # On duration
         toggle(LED1,GPIO.LOW)
         time.sleep(0.5)  # Off duration
-
-    
-'''
+            
 try:
-    while True:
-        if GPIO.input(MOISTURE_SENSOR_PIN) == GPIO.LOW:
-            print("Sensor detects WET soil 🌱💧")
-        else:
-            print("Sensor detects DRY soil 🌵🔥")
-        time.sleep(1)
-'''
-try:
+  '''Diode test'''
   blink_leds(2)
+  print("Diode test completed")
+  while True:
+      if GPIO.input(MOISTURE_SENSOR_PIN) == GPIO.LOW:
+          print("Sensor detects WET soil 🌱💧")
+          GPIO.output(WATER_POMP, GPIO.LOW)
+      else:
+          print("Sensor detects DRY soil 🌵🔥")
+          GPIO.output(WATER_POMP, GPIO.HIGH)
+      time.sleep(1)
   
 except KeyboardInterrupt:
     print("Stopping...")
